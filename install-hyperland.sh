62d1df0425a76e3323c59215e5bfc32186f99126
joan <joan@debook.168.1.18>
2024-09-08 16:14:03 +0200

    sacar comentarios

--- install-hyperland.sh
+++ install-hyperland.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/env bash
 
-#chmod +x ./src/*.sh
+chmod +x ./src/*.sh
 
-#bash ./src/dependencies.sh
-#bash ./src/setup-fonts.sh
+bash ./src/dependencies.sh
+bash ./src/setup-fonts.sh
 
-#cp ./assets/hyprland/hyprland.desktop /usr/share/wayland-sessions
+cp ./assets/hyprland/hyprland.desktop /usr/share/wayland-sessions
 
-#git clone --recursive https://github.com/hyprwm/Hyprland
-#cd Hyprland
-#make all && sudo make install
-#cd ..
+git clone --recursive https://github.com/hyprwm/Hyprland
+cd Hyprland
+make all && sudo make install
+cd ..
 
-#bash ./src/status-bar.sh
+bash ./src/status-bar.sh
 bash ./src/setup-audio-brightess-mgr.sh
-#bash ./src/setup-utils.sh
+bash ./src/setup-utils.sh
 
 
-#mkdir -p ~/.Wallpaper
-#mkdir -p ~/.config/hypr/
-#cp ./assets/fierwatchWallpaper.jpg ~/.Wallpaper
-#cp ./assets/hyprland/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
-#cp ./assets/hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
+mkdir -p ~/.Wallpaper
+mkdir -p ~/.config/hypr/
+cp ./assets/fierwatchWallpaper.jpg ~/.Wallpaper
+cp ./assets/hyprland/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+cp ./assets/hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
 
 #clean up
-#rm -fr aquamarine
-#rm -fr hyprlang
-#rm -fr hyprlock
-#rm -fr hyprcursor
-#rm -fr hyprpaper
-#rm -fr Hyprland
-#rm -fr hyprutils
-#rm -fr xdg-desktop-portal-hyprland
-#rm -fr hyprwayland-scanner
+rm -fr aquamarine
+rm -fr hyprlang
+rm -fr hyprlock
+rm -fr hyprcursor
+rm -fr hyprpaper
+rm -fr Hyprland
+rm -fr hyprutils
+rm -fr xdg-desktop-portal-hyprland
+rm -fr hyprwayland-scanner
