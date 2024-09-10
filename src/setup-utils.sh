a70f65d8cbfcde9a6da84ce3f7554e0788984e5c
Joan Esgleas <joan.esgleas@gmail.com>
2024-09-11 00:11:09 +0200

    arreglando cositas

--- src/setup-utils.sh
+++ src/setup-utils.sh
@@ -13,10 +13,15 @@ sudo apt install -y neovim ripgrep fd-find npm nodejs luarocks
 git clone https://github.com/Joan-Esgleas/nvim.git ~/.config/nvim
 
 wget https://github.com/Ulauncher/Ulauncher/releases/download/5.15.7/ulauncher_5.15.7_all.deb
-sudo apt install ./ulauncher*
+sudo apt install -y ./ulauncher*
 rm ulauncher*
 
 sudo apt install -y gnome-software flatpak
+
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+sudo apt install -y papirus-icon-theme
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
+
 sudo apt install -y gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --if-not-exists kdeapps https://distribute.kde.org/flatpak-apps-testing/
@@ -24,6 +29,3 @@ flatpak install -y --user --or-update https://cdn.kde.org/flatpak/xwaylandvideob
 flatpak install -y --or-update flathub org.onlyoffice.desktopeditors
 
 
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-sudo apt install -y papirus-icon-theme
-gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
