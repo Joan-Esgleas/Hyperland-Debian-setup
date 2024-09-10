e69de47f5f32a482faa5ae544f0c72f00e614f79
Joan Esgleas <joan.esgleas@gmail.com>
2024-09-10 23:39:32 +0200

    activamos sddm

--- src/setup-sddm.sh
+++ src/setup-sddm.sh
@@ -5,3 +5,5 @@ sudo cp ./assets/sddm/sddm.conf /etc/sddm.conf
 
 sudo apt install -y qml-module-qtquick-layouts qml-module-qtgraphicaleffects qml-module-qtquick-controls2 libqt5svg5
 sudo cp -r ./assets/sddm/sugar-candy /usr/share/sddm/themes/sugar-candy
+
+sudo systemctl enable sddm
