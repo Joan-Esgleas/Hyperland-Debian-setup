a7d61d12950453fc4fe5cba2da63c43596a0b426
Joan Esgleas <joan.esgleas@gmail.com>
2024-09-10 22:48:47 +0200

    sddm i utilitis

--- src/setup-utils.sh
+++ src/setup-utils.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
 
-sudo apt install -y gnome-software flatpak
-sudo apt install -y gnome-software-plugin-flatpak
-sudo apt install nautilus
-sudo apt install --no-install-recommends sddm
 sudo apt install -y firefox-esr
+sudo apt install nautilus
+
+sudo apt install -y dunst
+mkdir -p ~/.config/dunst/
+cp ./assets/dunst/dunstrc ~/.config/dunst/dunstrc
+
+sudo apt install -y alacritty
+git clone https://github.com/Joan-Esgleas/alacritty.git ~/.config/alacritty
+
+sudo apt install -y neovim ripgrep fd-find npm nodejs
+git clone https://github.com/Joan-Esgleas/nvim.git ~/.config/nvim
+
 wget https://github.com/Ulauncher/Ulauncher/releases/download/5.15.7/ulauncher_5.15.7_all.deb
 sudo apt install ./ulauncher*
+rm ulauncher*
+
+sudo apt install -y gnome-software flatpak
+sudo apt install -y gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --if-not-exists kdeapps https://distribute.kde.org/flatpak-apps-testing/
 flatpak install --user --or-update https://cdn.kde.org/flatpak/xwaylandvideobridge-nightly/org.kde.xwaylandvideobridge.flatpakref
 flatpak install --or-update flathub org.onlyoffice.desktopeditors
+
+
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 sudo apt install -y papirus-icon-theme
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
-rm ulauncher*
--- src/status-bar.sh
+++ src/status-bar.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 #dependencies
-sudo apt install -y waybar network-manager network-manager-gnome bluez-alsa-utils pamixer pipewire power-profiles-daemon
+sudo apt install -y waybar network-manager network-manager-gnome bluez-alsa-utils pamixer pipewire pulseaudio power-profiles-daemon
 
-cp -r ./assets/waybar ~/.config/
+mkdir -p ~/.config/waybar/
+cp ./assets/waybar/style.css ~/.config/waybar/style.css
+cp ./assets/waybar/config.jsonc ~/.config/waybar/config.jsonc
 
-#cleanup
-cd ..
