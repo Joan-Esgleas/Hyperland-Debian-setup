f7c3de0d69a2a872d357faf94cd5c1989f550b32
Joan Esgleas <joan.esgleas@gmail.com>
2024-09-11 15:09:52 +0200

    starship i el bashrc

--- src/setup-utils.sh
+++ src/setup-utils.sh
@@ -25,4 +25,12 @@ sudo apt install -y gnome-software-plugin-flatpak
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y --or-update flathub org.onlyoffice.desktopeditors
 
+if command -v "starship" >/dev/null 2>&1; then
+    echo "starship existe"
+else
+    curl -sS https://starship.rs/install.sh | sh
+fi
 
+sudo apt install bat
+cp ~/.bashrc ~/.bashrc.bkp
+cp ./assets/bashrc/.bashrc ~/.bashrc
