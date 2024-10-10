656f72596eb77fa40d67e164e4801a2a7ac0f615
Joan <joan@DebBookAir.168.1.18>
2024-10-10 18:25:47 +0200

    cositas

--- postinstall-hyperland.sh
+++ postinstall-hyperland.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
-
 sudo apt autoremove zutty
 sudo apt autoremove
 sudo reboot
