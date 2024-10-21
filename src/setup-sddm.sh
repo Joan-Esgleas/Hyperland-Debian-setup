622bddb08c6bb1bbacad2020dcee4f607ef16dda
Joan Esgleas <joan.esgleas@estudiantat.upc.edu>
2024-10-21 02:25:16 +0200

    arreglos

--- src/setup-sddm.sh
+++ src/setup-sddm.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-#sudo apt install -y --no-install-recommends sddm
+sudo apt install -y --no-install-recommends sddm
 sudo cp ./assets/sddm/sddm.conf /etc/sddm.conf
 
-sudo dnf install -y qml-module-qtquick-layouts qml-module-qtgraphicaleffects qml-module-qtquick-controls2 libqt5svg5
+sudo apt install -y qml-module-qtquick-layouts qml-module-qtgraphicaleffects qml-module-qtquick-controls2 libqt5svg5
 sudo mkdir -p /usr/share/sddm/themes/
 sudo git clone https://github.com/Joan-Esgleas/sugar-candy.git  /usr/share/sddm/themes/sugar-candy
 
-#sudo systemctl enable sddm
+sudo systemctl enable sddm
